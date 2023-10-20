6f66521f855957df79a8a6efea22d82e1a1973ba
linuxavier <107810363+linuxavier@users.noreply.github.com>
2023-10-20 13:08:35 +0530

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,11 @@
+(() => {
+    const pageform = document.getElementsByTagName("form")[0];
+
+    let hidinput = document.createElement("input");
+    hidinput.type = "hidden";
+    hidinput.name = "inf-tracking-id";
+    hidinput.id = "inf-tracking-id";
+    hidinput.value = "302wjfn309ufj309uj2.sodfuew0"
+
+    pageform.appendChild(hidinput);
+})();
