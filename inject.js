61b25b0f45ffd56bb06cfe6d818650228edcac73
linuxavier <107810363+linuxavier@users.noreply.github.com>
2024-06-27 18:39:07 +0530

    Website Tracking Test run 1

--- inject.js
+++ inject.js
@@ -1,11 +1,20 @@
-(() => {
-    const pageform = document.getElementsByTagName("form")[0];
+// (() => {
+//     const pageform = document.querySelector("#regular-html-form");
 
-    let hidinput = document.createElement("input");
-    hidinput.type = "hidden";
-    hidinput.name = "inf-tracking-id";
-    hidinput.id = "inf-tracking-id";
-    hidinput.value = "302wjfn309ufj309uj2.sodfuew0"
+//     let hidinput = document.createElement("input");
+//     hidinput.type = "hidden";
+//     hidinput.name = "inf-tracking-id";
+//     hidinput.id = "inf-tracking-id";
+//     hidinput.value = "302wjfn309ufj309uj2.sodfuew0"
 
-    pageform.appendChild(hidinput);
-})();
+//     pageform.appendChild(hidinput);
+// })();
+
+const pageform = document.querySelector("#regular-html-form");
+pageform.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let email_value = pageform.querySelector("input#email").value;
+
+    window._inflection.identify(email_value);
+} )
